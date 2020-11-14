f48fa3048f9175c60defda0dce4d50cac6853858
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-14 20:25:32 +0900

    nameカラムのバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
+
+  validates :name, presence: true
+  
   # def self.from_omniauth(auth)
   #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
   #     user.email = auth.info.email
