b34ccd7f1822bb90d6fa28fe00bc5b1be9b69962
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-14 20:23:37 +0900

    nameカラムの修正

--- db/migrate/20201114074747_add_columns_to_users.rb
+++ db/migrate/20201114074747_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :name, :string
+    add_column :users, :name, :string, null: false, default: ""
     add_column :users, :nickname, :string
     add_column :users, :location, :string
     add_column :users, :image, :string
