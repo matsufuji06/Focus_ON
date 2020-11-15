0a05b403fc1bde6994906a967c7585b90d0cb0b0
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-15 10:49:30 +0900

    editとupdateアクションの仮置き

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,6 +13,12 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
     def list_params
       params.require(:list).permit(:title).merge(user: current_user)
