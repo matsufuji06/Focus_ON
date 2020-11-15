8e5caa1c4bca38d66ca64d9d8e74999c51ca9e2c
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-15 10:47:54 +0900

    editとupdateアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root  'tops#index'
-  resources :lists, only: [:new, :create] 
+  resources :lists, only: [:new, :create, :edit, :update] 
 end
