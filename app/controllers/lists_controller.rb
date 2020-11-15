17e2f793829efc9edb7eee5bc905bd3189ca74ad
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-15 10:29:24 +0900

    LIstsコントローラーにnew&createアクションを仮設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,15 @@
 class ListsController < ApplicationController
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to :root
+    else
+      render action: :new
+    end
   end
 end
