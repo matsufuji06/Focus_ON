eab9b76a26e20c360b0bdacc7119124f971803ff
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-15 11:06:53 +0900

    destroyアクションのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root  'tops#index'
-  resources :lists, only: [:new, :create, :edit, :update] 
+  resources :lists, only: [:new, :create, :edit, :update, :destroy] 
 end
