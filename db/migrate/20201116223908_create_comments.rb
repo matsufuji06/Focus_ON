9997326124864ec9734653d7b633bcb5d8cd3e39
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-17 07:45:00 +0900

    コメントのマイグレーションファイルを編集、カラムを設定

--- db/migrate/20201116223908_create_comments.rb
+++ db/migrate/20201116223908_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.integer :list_id
+      t.integer :card_id
+      t.text :text
       t.timestamps
     end
   end
