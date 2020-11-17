0f6bc04caf30879b5e159d40518be7c98571c9ff
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-17 20:17:46 +0900

    ストロングパラメータの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,8 @@ class CommentsController < ApplicationController
     Comment.create(comment_params)
   end
 
-  
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(card_id: params[:card_id])
+  end
 end
