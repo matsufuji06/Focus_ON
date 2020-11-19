9dd14755d29e5ddcf9abe27dafe741e4cbf12dd1
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-20 07:42:09 +0900

    不要なコメントアウトの削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,51 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  # callback for facebook
-  # def facebook
-  #   callback_for(:facebook)
-  # end
-
   def facebook
     callback_from :facebook
   end
-  
-
-  # callback for twitter
-  # def twitter
-  #   callback_for(:twitter)
-  # end
-
 
   def twitter
     callback_from :twitter
   end
   
-  # callback for google
-  # def google_oauth2
-  #   callback_for(:google)
-  # end
-  
   def google_oauth2
     callback_from :google
   end
-  
-  
-  # common callback method
-  # def callback_for(provider)
-  #   @user = User.from_omniauth(request.env["omniauth.auth"])
-  #   if @user.persisted?
-  #     set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-  #     sign_in_and_redirect @user, event: :authentication
-  #   else
-  #     session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
-
-
-  # def failure
-  #   redirect_to root_path
-  # end
 
   private
   def callback_from(provider)
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token[0,20]
-  #   end
-  # end
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
 
